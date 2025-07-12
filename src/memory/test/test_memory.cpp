f1f478ab8087fdb332cc13042121f001328c9b6c
ocirnexam <kontakt.maxmueller@outlook.com>
2025-07-12 08:51:48 +0200

    Added unity test framework and tests for memory

---
+++ src/memory/test/test_memory.cpp
@@ -0,0 +1,57 @@
+#include <unity.h>
+
+#include "memory/memory.hpp"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_memory_init(void)
+{
+    Memory mem;
+    mem.Initialize();
+    for(int i = 0; i < mem.MAX_MEM; i++)
+    {
+        TEST_ASSERT_EQUAL(0, mem[i]);
+    }
+}
+
+void test_memory_set_byte(void)
+{
+    Memory mem;
+    mem.Initialize();
+    mem[0x100] = 0x7;
+    TEST_ASSERT_EQUAL(0x7, mem[0x100]);
+
+    mem[0x101] = 0x1111; // implicit cast wanted!
+    TEST_ASSERT_NOT_EQUAL_UINT16(0x1111, mem[0x101]);
+}
+
+void test_memory_set_word(void)
+{
+    Memory mem;
+    mem.Initialize();
+    mem.WriteWord(0x100, 0x1111);
+    TEST_ASSERT_EQUAL(0x1111, mem[0x100]);
+
+    mem.WriteWord(0x101, 0x11);
+    TEST_ASSERT_NOT_EQUAL(0x11, mem[0x101]);
+}
+
+int main(void)
+{
+	UNITY_BEGIN();
+
+	// TESTS
+	RUN_TEST(test_memory_init);
+	RUN_TEST(test_memory_set_byte);
+	RUN_TEST(test_memory_set_word);
+
+	UNITY_END();
+
+	return 0;
+}
